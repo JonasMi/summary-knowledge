96f216ca7f3de5c6f108ab1ce4e2a3b9d95eeaa8
PC-20180823HNHP\Administrator <912472323@qq.com>
2018-09-03 22:55:08 +0800

    idea git commit test

--- summary-knowledge-common/src/main/java/summary/knowledge/common/http/HttpMain.java
+++ summary-knowledge-common/src/main/java/summary/knowledge/common/http/HttpMain.java
@@ -12,26 +12,27 @@ import summary.knowledge.common.model.Person;
 
 public class HttpMain {
 
-	public static void main(String[] args) throws IOException {
-		ObjectMapper objectMapper = new ObjectMapper();
-		String url = "http://127.0.0.1:8080/test/get/?name= {name}";
-		/*
-		 * RestTemplate restTemplate = new RestTemplate();
-		 * restTemplate.getForObject("ur", Object.class);
-		 */
-		/* restTemplate.postForObject(url, request, responseType, uriVariables) */
-		RestTemplate restTemplate = new RestTemplate();
-		Map<String, Object> map = new HashMap<>();
-		map.put("name", "jonasMi");
-		map.put("sex", "男");
-		Person p = new Person();
-		p.setName("jonasMi");
-		p.setSex("男");
-		Person person = restTemplate.getForObject(url, Person.class, "name1","sex1");
-		
-		/*restTemplate.postForObject(url, request, responseType)*/
-		System.out.println(objectMapper.writeValueAsString(person));
+    public static void main(String[] args) throws IOException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        String url = "http://127.0.0.1:8080/test/get/?name= {name}";
+        /*
+         * RestTemplate restTemplate = new RestTemplate();
+         * restTemplate.getForObject("ur", Object.class);
+         */
+        /* restTemplate.postForObject(url, request, responseType, uriVariables) */
+        RestTemplate restTemplate = new RestTemplate();
+        RestTemplate restTemplate1 = new RestTemplate();
+        Map<String, Object> map = new HashMap<>();
+        map.put("name", "jonasMi");
+        map.put("sex", "男");
+        Person p = new Person();
+        p.setName("jonasMi");
+        p.setSex("男");
+        Person person = restTemplate.getForObject(url, Person.class, "name1", "sex1");
+
+        /*restTemplate.postForObject(url, request, responseType)*/
+        System.out.println(objectMapper.writeValueAsString(person));
 		/*URL url = new URL("http://127.0.0.1:8080/test/get/");
 		URLConnection connection = url.openConnection();*/
-	}
+    }
 }
